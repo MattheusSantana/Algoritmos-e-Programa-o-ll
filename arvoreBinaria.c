a063b0eea3dc8ad640c0bd0ea119aa47ba95dbc9
MattheusSantana <matheusess@hotmail.com>
2017-01-24 16:28:09 -0200

    Implementação das funções

--- arvoreBinaria.c
+++ arvoreBinaria.c
@@ -31,13 +31,13 @@ int main(){
 		for(i =0; i < MAX; i++)
 		printf("arvore ordenada ----> %d \n", v4[i]);
 	
-	return 0;	//Fim main.
+	return 0;
 }
 
 
 int pai(int i){
-	if(i == 0)					//Caso esteja na raiz.
-		return 0;				//Retorne a própria raiz.
+	if(i == 0)				//Caso esteja na raiz.
+		return 0;			//Retorne a própria raiz.
 	else
 		return (i -1) /2;		//Retornando o nó pai.
 }
@@ -50,22 +50,20 @@ int direito(int i){
 	return 2 * (i+1); 
 }
 
-/*Manutenção da propriedade max-heap*/
-
 void desce(int n, int v[MAX], int i){
 	int e = esquerdo(i);			//Obtendo filho da esquerda.
-	int d = direito(i);				//Obterndo filho da direita.
+	int d = direito(i);			//Obterndo filho da direita.
 	int maior;		
 
 	if(e < n && v[e] > v[i])		//Se o filho da esquerda existe, e se o filho é maior que o pai.
 		maior = e;
 	else
 		maior = i;
-	if(d < n && v[d] > v[maior])	//Se o filho da direita existe e é maior do que o maior.
+	if(d < n && v[d] > v[maior])		//Se o filho da direita existe e é maior do que o maior.
 		maior = d;
-	if(maior != i){					//Caso o maior dos elementos não seja o próprio pai.	
+	if(maior != i){				//Caso o maior dos elementos não seja o próprio pai.	
 		troca(&v[i], &v[maior]);	//Trocando os elementos de posição.   
-		desce(n, v, maior);			//Chamando a função recursivamente, passando o maior como i de forma que ele desça até a sua posição correta.
+		desce(n, v, maior);		//Chamando a função recursivamente, passando o maior como i de forma que ele desça até a sua posição correta.
 	}
 }
 int troca(int *a, int *b){
@@ -78,19 +76,18 @@ int troca(int *a, int *b){
 void constroi_MaxHeap(int n, int v[]){
 	int i;
 	for(i = n/2 -1; i >= 0; i--)  	//Começando do ultimo elemento da penultima linha da árvore e indo até a raiz.
-		desce(n, v, i);				//Organizando o elemento da vez de forma a satisfazer a propriedade max heap.
+		desce(n, v, i);			//Organizando o elemento da vez de forma a satisfazer a propriedade max heap.
 }
 
 void altera_MaxHeap(int n, int v[], int i){
 	if(v[i] < v[esquerdo(i)] || v[i] < v[direito(i)])	//Caso o elemento inserido seja menor do que seus filhos
 		desce(n, v, i);						//Desça esse elemento.
 
-	if(v[i] > v[pai(i)])					//Caso o elemento em i seja maior do que o elemento pai em i.
+	if(v[i] > v[pai(i)])						//Caso o elemento em i seja maior do que o elemento pai em i.
 		sobe(i);							//Subindo esse elemento.		
 }
 
 void sobe(int n, int v[], int i){
-		  
 		while(v[pai(i)] < v[i]){				//Enquanto o elemento é maior do que o elemento pai.
 		troca(&v[i], &v[pai(i)]);				//Troque.
 		 	i = pai(i);						//Atualizando o valor de i para verificar novamente. 		
